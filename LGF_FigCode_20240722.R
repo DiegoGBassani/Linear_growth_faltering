6ad85248b4c4366b297412538f01f456d9611183
DiegoGBassani <diego.bassani@sickkids.ca>
2024-10-15 18:59:16 +0200

    Update and rename DR_KW_DB_FigCode_20240208.R to LGF_FigCode_20240722.R
    Signed-off-by: DiegoGBassani <diego.bassani@sickkids.ca>

--- LGF_FigCode_20240722.R
+++ LGF_FigCode_20240722.R
@@ -1,12 +1,10 @@
 
 ########################################################################
 ## PROJECT: Response to HBGDki consortium 2023 paper #1
-## PROGRAM: DR_KW_DB_FigCode_20240208
+## PROGRAM: LGF_FigCode_20240722
 ## TASK: Generate main figures
 ## CREATED BY: Kelly Watson, The Hospital for Sick Children
-## DATE: Feb 08, 2024
-## Updated: Jun 25th 2024
-## Update note: Changing file name.
+## DATE: July 22, 2023
 
 ####################################################################
 ## Load necessary packages
@@ -25,7 +23,8 @@ require(gridExtra)
 ### Figure 1 Code
 
 # Load data for Fig2a
-Fig1 <- read_excel("DR_KW_DB_Fig1_Rdata_20240208.xlsx")
+library(readxl)
+Fig1 <- read_excel("~/Desktop/LGF_Fig1_Rdata_240722.xlsx")
 
 # Set variables as factors
 Fig1$Metric_factor <- as.factor(Fig1$Metric)
@@ -70,7 +69,7 @@ Fig1Plot <- ggplot() +
         legend.position = c(0.8,0.8))
 
 
-ggsave("DR_KW_DB_Fig1_20240208.jpg", plot = Fig1Plot, units = "in", dpi = 300, width = 5, height = 5)
+ggsave("LGF_Fig1_20240722.jpg", plot = Fig1Plot, units = "in", dpi = 300, width = 5, height = 5)
 
 ####################################################################
 
@@ -78,7 +77,7 @@ ggsave("DR_KW_DB_Fig1_20240208.jpg", plot = Fig1Plot, units = "in", dpi = 300, w
 ### Figure 2 Code
 
 # Load data for Fig2a
-Fig2a <- read_excel("DR_KW_DB_MattersArising_Fig2a_Rdata_20240208.xlsx")
+Fig2a <- read_excel("~/Desktop/LGF_Fig2a_Rdata_240722.xlsx")
 Fig2a$change_laz_factor <- as.factor(Fig2a$change_laz)
 Fig2a$laz0_factor <- as.factor(Fig2a$laz0)
 
@@ -110,7 +109,7 @@ Figure2a <- ggplot(Fig2a) +
   scale_fill_manual(name = "", labels = c("Incident stunting onset", "Stunting reversal"), values = c("#332288", "#88CCEE"))
 
 # Load data for Fig2b
-Fig2b <- read_excel("DR_KW_DB_Fig2b_Rdata_20240208.xlsx")
+Fig2b <- read_excel("~/Desktop/LGF_Fig2b_Rdata_240722.xlsx")
 Fig2b$corr_factor <- as.factor(Fig2b$correlation)
 Fig2b$laz0_factor <- as.factor(Fig2b$LAZ0)
 
@@ -145,12 +144,90 @@ Figure2b <- ggplot(Fig2b) +
 combined_plot <- ggarrange(Figure2a, Figure2b, nrow=2, labels = c("a", "b"), common.legend = TRUE, legend="right")
 
 # Save the combined plot as a jpeg
-ggsave("DR_KW_DB_Fig2_20240208.jpg", plot = combined_plot, units = "in", dpi = 300, width = 10, height = 5)
+ggsave("LGF_Fig2_240722.jpg", plot = combined_plot, units = "in", dpi = 300, width = 10, height = 5)
+
+
+####################################################################
 
 ####################################################################
 ### Figure 3 Code
 
-SA_monthly <- read_excel("DR_KW_DB_MattersArising_Fig3_Rdata_20240208.xlsx")
+# Load data for Fig3a
+library(readxl)
+Fig3a <- read_excel("LGF_Fig3a_Rdata_240722.xlsx")
+Fig3a$change_laz_factor <- as.factor(Fig3a$change_laz)
+Fig3a$laz0_factor <- as.factor(Fig3a$laz0)
+
+# Create 3a plot
+Fig3a<- ggplot(Fig3a) +
+  geom_point(aes(x = laz0_factor, y = Ratio, colour = Metric)) +
+  facet_grid(~change_laz_factor) +
+  theme(panel.grid.major.y = element_line(color = "grey90",
+                                          size = 0.3),
+        strip.background = element_rect(fill='grey', size=0.5),
+        strip.text = element_text(size = 8, face = 'bold'),
+        panel.spacing = unit(0.09, "cm"),
+        plot.background = element_rect(fill = "#FFFFFF"),
+        panel.background = element_rect(fill = "#FFFFFF"),
+        panel.border = element_blank(),
+        axis.line = element_line(colour = 'black'),
+        axis.title = element_text(size = 8),
+        legend.title = element_text(size = 8),
+        legend.text = element_text(size = 8),
+        axis.text.y = element_text(size = 8),
+        axis.text.x = element_text(size = 8),
+        plot.title = element_text(hjust = 0.5, size=8, face='bold')) +
+  xlab("") +
+  ylab("Ratio (reference LAZ change = 0)") +
+  ylim(1, 3) +
+  ggtitle('Change in mean LAZ') +
+  scale_colour_manual(name = "", labels = c("Incident stunting onset", "Stunting reversal"), values = c("#332288", "#88CCEE"))
+
+################################
+
+# Load data for Fig3b
+Fig3b <- read_excel("LGF_Fig3b_Rdata_240722.xlsx")
+Fig3b$corr_factor <- as.factor(Fig3b$correlation)
+Fig3b$laz0_factor <- as.factor(Fig3b$LAZ0)
+
+# Create Ext Data Fig1b plot
+Fig3b<- ggplot(Fig3b) +
+  geom_point(aes(x = laz0_factor, y = Ratio, colour = Metric)) +
+  facet_grid(~corr_factor) +
+  theme(panel.grid.major.y = element_line(color = "grey90",
+                                          size = 0.3),
+        strip.background = element_rect(fill='grey', size=0.5),
+        strip.text = element_text(size = 8, face = 'bold'),
+        panel.spacing = unit(0.09, "cm"),
+        plot.background = element_rect(fill = "#FFFFFF"),
+        panel.background = element_rect(fill = "#FFFFFF"),
+        panel.border = element_blank(),
+        axis.line = element_line(colour = 'black'),
+        axis.title = element_text(size = 8),
+        legend.title = element_text(size = 8),
+        legend.text = element_text(size = 8),
+        axis.text.y = element_text(size = 8),
+        axis.text.x = element_text(size = 8),
+        plot.title = element_text(hjust = 0.5, size=8, face='bold')) +
+  xlab("Starting mean LAZ") +
+  ylab("Ratio (reference corr = 0.9)") +
+  ylim(1, 3) +
+  ggtitle('Between-timepoint correlation') +
+  scale_colour_manual(name = "", labels = c("Incident stunting onset", "Stunting reversal"), values = c("#332288", "#88CCEE"))
+
+#################
+combined_plot <- ggarrange(Fig3a, Fig3b, nrow=2, labels = c("a", "b"), common.legend = TRUE, legend="right")
+
+# Save the combined plot as a jpeg
+ggsave("LGF_Fig3_240722.jpg", plot = combined_plot, units = "in", dpi = 300, width = 8, height = 6)
+
+
+####################################################################
+
+####################################################################
+### Figure 4 Code
+
+SA_monthly <- read_excel("LGF_Fig4_Rdata_240722.xlsx")
 
 SA_LAZ_age <- ggplot(data=SA_monthly, aes(x=age_mos, y=m_laz))+
   geom_smooth(se=FALSE, size = 1, colour = 'forestgreen')+
@@ -207,70 +284,6 @@ SA_GD_age <- ggplot(data=SA_monthly, aes(x=age_mos, y=GD_mos))+
         axis.text.y = element_text(size = 8),
         axis.text.x = element_text(size = 8))
 
-Fig3 <- ggarrange(SA_LAZ_age, SA_stunt_age, SA_GD_age, labels = c("a", "b","c"),ncol=3)
+Fig4 <- ggarrange(SA_LAZ_age, SA_stunt_age, SA_GD_age, labels = c("a", "b","c"),ncol=3)
 
-ggsave("DR_KW_DB_Fig3_20240208.jpg", plot = Fig3, units = "in", dpi = 300, width = 10, height = 2.5)
-
-####################################################################
-### Figure 4 Code
-
-fig4 <- read_excel("DR_KW_DB_MattersArising_Fig4_Rdata_20240208.xlsx")
-
-fig4_LAZ_age <- ggplot(data=fig4, aes(x=age_mos, y=m_laz))+
-  geom_smooth(se=FALSE, size = 1, colour = 'forestgreen')+
-  ylim(-2,-0.5)+
-  xlab("Child age (months)")+
-  ylab("Length-for-age Z-score")+
-  theme(panel.grid.major.y = element_line(color = "grey90",
-                                          size = 0.3),
-        plot.background = element_rect(fill = "#FFFFFF"),
-        panel.background = element_rect(fill  = "#FFFFFF"),
-        panel.border = element_blank(),
-        axis.line = element_line(colour='black'),
-        axis.title = element_text(size = 8),
-        legend.title = element_text(size = 8),
-        legend.text = element_text(size = 8),
-        axis.text.y = element_text(size = 8),
-        axis.text.x = element_text(size = 8))
-
-fig4$stunt_perct <- fig4$stunt_prop * 100
-
-fig4_stunt_age <-ggplot(data=fig4, aes(x=age_mos, y=stunt_perct))+
-  geom_smooth(se=FALSE, size = 1, colour = 'royalblue')+
-  ylim(0,100)+
-  xlab("Child age (months)")+
-  ylab("Stunting Prevalence (%)")+
-  theme(panel.grid.major.y = element_line(color = "grey90",
-                                          size = 0.3),
-        plot.background = element_rect(fill = "#FFFFFF"),
-        panel.background = element_rect(fill  = "#FFFFFF"),
-        panel.border = element_blank(),
-        axis.line = element_line(colour='black'),
-        axis.title = element_text(size = 8),
-        legend.title = element_text(size = 8),
-        legend.text = element_text(size = 8),
-        axis.text.y = element_text(size = 8),
-        axis.text.x = element_text(size = 8))
-
-fig4$GD_mos <- fig4$GD / 30.4375
-
-fig4_GD_age <- ggplot(data=fig4, aes(x=age_mos, y=GD_mos))+
-  geom_smooth(se=FALSE, size = 1, colour = 'maroon')+
-  ylim(0,6)+
-  xlab("Child age (months)")+
-  ylab("Growth Delay (months)")+
-  theme(panel.grid.major.y = element_line(color = "grey90",
-                                          size = 0.3),
-        plot.background = element_rect(fill = "#FFFFFF"),
-        panel.background = element_rect(fill  = "#FFFFFF"),
-        panel.border = element_blank(),
-        axis.line = element_line(colour='black'),
-        axis.title = element_text(size = 8),
-        legend.title = element_text(size = 8),
-        legend.text = element_text(size = 8),
-        axis.text.y = element_text(size = 8),
-        axis.text.x = element_text(size = 8))
-
-Fig4 <- ggarrange(fig4_LAZ_age, fig4_stunt_age, fig4_GD_age, labels = c("a", "b","c"),ncol=3)
-
-ggsave("DR_KW_DB_Fig4_20240208.jpg", plot = Fig4, units = "in", dpi = 300, width = 10, height = 2.5)
+ggsave("LGF_Fig4_240722.jpg", plot = Fig4, units = "in", dpi = 300, width = 10, height = 2.5)
